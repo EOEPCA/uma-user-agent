afabac81ccf55949eaaae0c0e540a4863e3ccf4c
Richard Conway <richard@rconway.co.uk>
2021-09-24 12:06:08 +0100

    Corrected logic error

--- pkg/uma/uma-client.go
+++ pkg/uma/uma-client.go
@@ -60,7 +60,7 @@ func (umaClient *UmaClient) ExchangeTicketForRpt(requestLogger *logrus.Entry, au
 	err = nil
 
 	// Check we have a User ID Token
-	if len(userIdToken) > 0 {
+	if len(userIdToken) == 0 {
 		err = fmt.Errorf("missing User ID Token to exchange ticket for RPT")
 		requestLogger.Error(err)
 		return
