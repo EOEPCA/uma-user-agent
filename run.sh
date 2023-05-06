1f5c309d64945a5d8bc375c8335c078603f379d0
Richard Conway <richard@rconway.co.uk>
2023-05-06 20:52:23 +0100

    Remove unused env vars from scripts

--- run.sh
+++ run.sh
@@ -7,15 +7,10 @@ BIN_DIR="$(pwd)"
 onExit() {
   cd "${ORIG_DIR}"
 }
-
 trap onExit EXIT
 
 go install github.com/cosmtrek/air@latest
 
-export PORT=8080
-export LOG_LEVEL=trace
-export CLIENT_ID="22ba0c56-9780-4b0b-ad71-d745c166ca3b"
-export CLIENT_SECRET="0e3e1d0d-9002-4d44-bbff-a170efa18512"
 export CONFIG_DIR="./"
 
 air
--- test.sh
+++ test.sh
@@ -7,12 +7,7 @@ BIN_DIR="$(pwd)"
 onExit() {
   cd "${ORIG_DIR}"
 }
-
 trap onExit EXIT
-
-export LOG_LEVEL=trace
-export CLIENT_ID="528e4b49-1f6f-4ff7-bc61-36d08b00c69a"
-export CLIENT_SECRET="8afaadc0-d09b-4b28-bedc-765dcf5c27f4"
 
 GOFLAGS="-count=1" go test "$@" ./...
 let status=$?
